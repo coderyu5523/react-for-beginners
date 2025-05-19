554e65f839e104b71c19fc2163290c20b38c0188
coderyu5523 <coderyu5523@naver.com>
2025-05-19 20:41:03 +0900

    cleanup funcion

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import {useState, useEffect} from "react";
 
+function Hello(){
+  function byeFn(){
+    console.log("bye :(")
+  }
+
+
+  function hiFn (){
+    console.log("created :)");
+    return byeFn; //cleanup function
+
+
+  }
+  useEffect(()=>{
+    console.log("hi :)")
+    return () => console.log("bye :(")
+  },[])
+  useEffect(function(){
+    console.log("hi :)")
+    return function(){
+      console.log("bye :(")
+    }
+  },[])
+
+  // useEffect(()=>{
+  //   console.log("created :)");
+  //   //cleanup function, 
+  //   return () => console.log("destroyd :("); 
+  // },[]);
+  return (
+  <h1>
+    Hello
+  </h1>
+  )
+}
 
 function App() {
-  const [counter,setValue] = useState(0);
-  const [keyword,setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev +1);
-  const onChange = (event) => setKeyword(event.target.value);
-  
-  useEffect(()=>{
-    console.log("i run only once");
-   },[]); // []가 비어있으면 아무것도 지켜볼게 없기 때문에 한 번만 실행됨.
-   useEffect(()=>{
-    console.log("i run when 'keyword' changes")
-    },[keyword]); // keyword가 변화할 때 코드를 실행
-    useEffect(()=>{
-    console.log("i run when 'counter' changes")
-    },[counter]);
-    useEffect(()=>{
-      console.log("i run when keyword & counter changes")
-    },[keyword,counter]);
+  const [showing,setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholer="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>
+        {showing ? "Hide" : "Show"}
+      </button>
     </div>
   );
 }
