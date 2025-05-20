e4f1e2d140c958fb9c0286d9fc14ea3ce53bab61
coderyu5523 <coderyu5523@naver.com>
2025-05-20 19:29:20 +0900

    ... 추가

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
 import {useState, useEffect} from "react";
 
-function Hello(){
-  function byeFn(){
-    console.log("bye :(")
-  }
 
-
-  function hiFn (){
-    console.log("created :)");
-    return byeFn; //cleanup function
-
-
-  }
-  useEffect(()=>{
-    console.log("hi :)")
-    return () => console.log("bye :(")
-  },[])
-  useEffect(function(){
-    console.log("hi :)")
-    return function(){
-      console.log("bye :(")
-    }
-  },[])
-
-  // useEffect(()=>{
-  //   console.log("created :)");
-  //   //cleanup function, 
-  //   return () => console.log("destroyd :("); 
-  // },[]);
-  return (
-  <h1>
-    Hello
-  </h1>
-  )
-}
 
 function App() {
-  const [showing,setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo,setToDo] = useState("");
+  const [toDos,setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if(toDo === ""){
+      return ;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo,...currentArray] );
+
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>
-        {showing ? "Hide" : "Show"}
-      </button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write youre to do..." />
+        <button>Add ToDo</button>
+      </form>
+  
     </div>
   );
 }
